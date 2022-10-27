ad3f8eb9bda88528f4c79a77831393b3358bc19c
Irina <115542977+IrinaGaripova@users.noreply.github.com>
2022-10-27 17:52:16 +0500

    add Example006 Buse

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,12 +1,12 @@
-﻿Console.Write('Введите имя пользователя')
+﻿Console.Write("Введите имя пользователя");
 string username = Console.ReadLine();
 
-if(username == 'Маша')
+if(username == "Маша")
 {
-    Console.WriteLine('Ура это же Маша');
+    Console.WriteLine("Ура это Маша");
 }
 else
 {
-    Console.Write('Привет, ');
+    Console.Write("Привет, ");
     Console.WriteLine(username);
 }
